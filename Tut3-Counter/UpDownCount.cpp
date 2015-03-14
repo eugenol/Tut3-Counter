3fab9b177eceaaf771055d5089fa589935ab7f48
eugenol <eug@live.co.za>
2015-03-14 18:40:25 +0200

    resolve conflicts

--- Tut3-Counter/UpDownCount.cpp
+++ Tut3-Counter/UpDownCount.cpp
@@ -3,13 +3,8 @@
 
 UpDownCount::UpDownCount(int low, int high, int inc, int dec)
 {
-<<<<<<< HEAD
-	//Initialize the counter.
-	count = lowVal = low;
-=======
 	count = low; //Counter starts counting from the lowest count
 	lowVal = low;
->>>>>>> a6076bb2faa283c167b730a0594e2c2b4a3f7033
 	highVal = high;
 	incVal = inc;
 	decVal = dec;
@@ -24,12 +19,8 @@ UpDownCount UpDownCount::operator++()
 {
 	UpDownCount Temp(lowVal, highVal, incVal, decVal);
 
-<<<<<<< HEAD
 	// If the counter will not exceed the maximum count, increment the counter.
-	if (count + incVal <= highVal)
-=======
-	if (count + incVal <= highVal) // Cheeck if incrementation will exceed the maximum value
->>>>>>> a6076bb2faa283c167b730a0594e2c2b4a3f7033
+	if (count + incVal <= highVal) // Check if incrementation will exceed the maximum value
 	{
 		count += incVal;
 	}
@@ -45,13 +36,8 @@ UpDownCount UpDownCount::operator++(int)
 	UpDownCount Temp(lowVal, highVal, incVal, decVal);
 	Temp.count = count;
 
-<<<<<<< HEAD
 	// If the counter will not exceed the maximum count, increment the counter.
-	if (count + incVal <= highVal)
-	{
-=======
 	if (count + incVal <= highVal) // Cheeck if incrementation will exceed the maximum value
->>>>>>> a6076bb2faa283c167b730a0594e2c2b4a3f7033
 		count += incVal;
 	else
 		std::cout << "Can not increment the counter" << std::endl;
@@ -62,14 +48,9 @@ UpDownCount UpDownCount::operator--()
 {
 	UpDownCount Temp(lowVal, highVal, incVal, decVal);
 
-<<<<<<< HEAD
-	// If the counter will be lower than the minimum count, decrement the counter.
-	if (count - decVal >= lowVal)
-	{
-=======
 
+	// If the counter will be lower than the minimum count, decrement the counter.
 	if (count - decVal >= lowVal) // Cheeck if decrementation will be lower than  the minimum value
->>>>>>> a6076bb2faa283c167b730a0594e2c2b4a3f7033
 		count -= decVal;
 	else
 		std::cout << "Can not decrement the counter" << std::endl;
@@ -83,13 +64,8 @@ UpDownCount UpDownCount::operator--(int)
 	UpDownCount Temp(lowVal, highVal, incVal, decVal);
 	Temp.count = count;
 
-<<<<<<< HEAD
 	// If the counter will be lower than the minimum count, decrement the counter.
-	if (count - decVal >= lowVal)
-	{
-=======
 	if (count - decVal >= lowVal) // Cheeck if decrementation will be lower than  the minimum value
->>>>>>> a6076bb2faa283c167b730a0594e2c2b4a3f7033
 		count -= decVal;
 	else
 		std::cout << "Can not decrement the counter" << std::endl;
